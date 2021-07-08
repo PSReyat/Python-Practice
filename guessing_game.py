4775159bfe923a01b3055cc3a458b8e3c6c2f8cb
Parminder <preyat94@gmail.com>
2021-07-08 10:00:36 +0100

    slight changes to guessing_game

--- guessing_game.py
+++ guessing_game.py
@@ -1,12 +1,19 @@
 target = 9
 
 guess = int(input("Guess: "))
+count = 1
 
 while guess is not target:
     if guess < target:
         print("A bit higher")
     elif guess > target:
         print("A bit lower")
+    elif guess is target:
+        print("Correct!")
+        break
     guess = int(input("Guess: "))
+    count = count + 1
+    if count == 5:
+        print("You lost...")
+        break
 
-print("Correct!")
